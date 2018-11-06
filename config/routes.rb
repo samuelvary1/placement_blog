dd90c2dfe327fbc443c912b72956756ac748eb24
samuelvary1 <svary@triplelift.com>
2018-11-06 13:09:59 -0500

    finished up comments architecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  # creates the nested relationship of comments belonging to articles
+  resources :articles do
+    resources :comments
+  end
 
-  
+
   root 'welcome#index'
 end
